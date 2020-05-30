7ee72d2b8a93cb8d1add26c111160132b0079904
Matías Cabrera <matiasrodrigocabrera@gmail.com>
2020-05-30 00:38:15 -0300

    Se agrega conexion a la base de datos

--- fulbito-server/index.js
+++ fulbito-server/index.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var app = express();
+var mysql = require('mysql');
 
+// SERVER
 app.get('/', function (req, res) {
   res.send('Hello World!');
 });
@@ -8,3 +10,22 @@ app.get('/', function (req, res) {
 app.listen(8081, function () {
   console.log('Server listening on port 8081!');
 });
+
+// DATABASE
+var connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: 'password',
+  database: 'fulbito'
+})
+
+connection.connect(
+  function(error) {
+    if(!!error) {
+      console.log('Error')
+    }
+    else {
+      console.log('Database connected')
+    }
+  }
+)
