24e49af47836a7b1f0221823c4e1f3d51e2a8d40
Ger121190 <german.cabrera@connectis-ict.com.ar>
2020-06-10 18:42:30 -0300

    Agregado mock server para caso de test

--- fulbito-server/features/support/steps.js
+++ fulbito-server/features/support/steps.js
@@ -1,20 +1,40 @@
 const { Given, When, Then } = require("cucumber");
-const axios = require('axios');
-const assert = require('assert');
+const axios = require("axios");
+const assert = require("assert");
+const server = require("express-mock-server");
+const mock = require("../../mock/mock");
 
-let partido = { fecha: '', hora: '', lugar: '' };
+let sources = [mock];
+
+let serverConfig = {
+  port: 8081
+};
+
+let serverMock = server.serverStart(sources, serverConfig);
+
+let partido = { fecha: "", hora: "", lugar: "" };
 let response;
 
-Given('un usuario crea un partido con fecha {string} hora {string} y lugar {string}', function(fecha, hora, lugar) {
-  partido = { fecha, hora, lugar }
-})
+Given(
+  "un usuario crea un partido con fecha {string} hora {string} y lugar {string}",
+  function (fecha, hora, lugar) {
+    partido = { fecha, hora, lugar };
+  }
+);
 
-When('guarda la información', async function() {
-  response = await axios.post('http://127.0.0.1:8081/partidos', partido)
-})
+When("guarda la información", async function () {
+  response = await axios.post("http://127.0.0.1:8081/partidos", partido);
+});
 
-Then('el partido queda definido con fecha {string} hora {string} y lugar {string}', function(fecha, hora, lugar) {
-  assert.equal(response.data.fecha, fecha)
-  assert.equal(response.data.hora, hora)
-  assert.equal(response.data.lugar, lugar)
-})
+Then(
+  "el partido queda definido con fecha {string} hora {string} y lugar {string}",
+  function (fecha, hora, lugar) {
+    assert.equal(response.data.fecha, fecha);
+    assert.equal(response.data.hora, hora);
+    assert.equal(response.data.lugar, lugar);
+
+    serverMock.close();
+  }
+);
+
+
