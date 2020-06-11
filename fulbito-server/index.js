50ec0ee7f6cd4208d8e3003d0a9bdd10f19d7a71
Matías Cabrera <matiasrodrigocabrera@gmail.com>
2020-06-11 19:46:10 -0300

    Se agrega controller a la estructura del proyecto

--- fulbito-server/index.js
+++ fulbito-server/index.js
@@ -1,16 +1,21 @@
-var express = require("express");
-var bodyParser = require("body-parser");
+const express = require("express");
+const bodyParser = require("body-parser");
 const cors = require('cors')
 const partidoRouter = require('./routers/partidoRouter')
-var app = express();
+
+// DATABASE
+const databaseConnection = require("./database/connection")
 
 // SERVER
+const app = express();
+
 app.use(bodyParser.json());
 app.use(cors());
-app.use('/partidos', partidoRouter)
 
 var port = process.env.port || 8081;
 
 app.listen(port, function () {
   console.log("Server listening on port 8081!");
 });
+
+app.use('/partidos', partidoRouter)
