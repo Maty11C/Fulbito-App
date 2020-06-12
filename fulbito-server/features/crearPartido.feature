279c75af12f4561edab4dbddeb15207eafae343a
Matías Cabrera <matiasrodrigocabrera@gmail.com>
2020-06-11 21:30:40 -0300

    Se crea test de creacion de partido con fecha invalida

--- fulbito-server/features/crearPartido.feature
+++ fulbito-server/features/crearPartido.feature
@@ -7,3 +7,8 @@ Feature: Crear un partido
         Given un usuario crea un partido con fecha '2020-06-16' hora '19:00:00' y lugar 'Libertadores de América'
         When guarda la información
         Then el partido queda definido con fecha '2020-06-16' hora '19:00:00' y lugar 'Libertadores de América'
+
+    Scenario: Creacion de un partido con fecha invalida
+        Given un usuario crea un partido con fecha '1986-06-16' hora '19:00:00' y lugar 'Libertadores de América'
+        When guarda la información
+        Then el partido no se crea
--- fulbito-server/features/support/steps.js
+++ fulbito-server/features/support/steps.js
@@ -3,7 +3,7 @@ const axios = require("axios");
 const assert = require("assert");
 
 let partido = { fecha: "", hora: "", lugar: "" };
-let response;
+let dataResponse;
 
 Given(
   "un usuario crea un partido con fecha {string} hora {string} y lugar {string}",
@@ -13,16 +13,27 @@ Given(
 );
 
 When("guarda la información", async function () {
-  response = await axios.post("http://127.0.0.1:8081/partidos", partido);
+  const res = await axios.post("http://localhost:8081/partidos", partido)
+  .then(response => {
+    dataResponse = response.data
+  })
+  .catch(error => {
+    dataResponse = error.response.data
+  });
 });
 
 Then(
   "el partido queda definido con fecha {string} hora {string} y lugar {string}",
   function (fecha, hora, lugar) {
-    assert.equal(response.data.fecha, fecha);
-    assert.equal(response.data.hora, hora);
-    assert.equal(response.data.lugar, lugar);
+    assert.equal(dataResponse.fecha, fecha);
+    assert.equal(dataResponse.hora, hora);
+    assert.equal(dataResponse.lugar, lugar);
   }
 );
 
-
+Then(
+  "el partido no se crea",
+  function () {
+    assert.equal(dataResponse.message, 'La fecha es inválida');
+  }
+);
